8bff08b63ffb783f903991fe74d00ef69796f5e6
acrostm <zjc971109@gmail.com>
2024-02-05 04:05:21 -0800

    introduced TypeOrm framework

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import envConfig from '../config/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 设置为全局
+      envFilePath: [envConfig.path],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // 数据库类型
+        entities: [], // 数据表实体
+        host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT', 3306), // 端口号
+        username: configService.get('DB_USER', 'root'), // 用户名
+        password: configService.get('DB_PASSWORD', 'root'), // 密码
+        database: configService.get('DB_DATABASE', 'blog'), //数据库名
+        timezone: '+08:00', //服务器上配置的时区
+        synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
