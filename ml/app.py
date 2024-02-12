a19724552014a595ec9be6d2475f862660f179dd
KetianBai <85915101+KetianBai@users.noreply.github.com>
2024-02-12 00:46:39 -0800

    Update app.py

--- ml/app.py
+++ ml/app.py
@@ -1,12 +1,13 @@
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 import cv2
 import base64
 import numpy as np
-import body  # 导入body模块
+import face
 
 app = Flask(__name__)
-cros:socketio = SocketIO(app,port=5001)
+socketio = SocketIO(app, port=5001, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
@@ -15,6 +16,8 @@ def index():
 @socketio.on('image')
 def handle_image(data):
     # 解码图像
+    
+
     nparr = np.frombuffer(data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     if img is None or img.size == 0:
@@ -23,7 +26,7 @@ def handle_image(data):
     
 
     # 使用 face.py 处理图像
-    processed_img = body.process_frame(img)
+    processed_img = face.process_frame(img)
 
     # 将处理后的图像编码并发送回前端
     _, buffer = cv2.imencode('.jpg', processed_img)
@@ -38,6 +41,3 @@ def handle_image(data):
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5001)
-
-
-
