474753cb14bb51c09920f0cf579cb876e93a1740
acrostm <zjc971109@gmail.com>
2024-03-20 23:40:15 -0700

    [WIP] testing on video upload

--- test/upload_video_code/app.py
+++ test/upload_video_code/app.py
@@ -55,37 +55,37 @@
 #     app.run(port=5001)  # 启动 Flask 服务器，端口设为 5001
 # import cv2
 # import face
-
+#
 # # 读取本地视频文件
 # video_path = 'https://r2.3cap.xyz/2820_1710813261.mp4'
 # cap = cv2.VideoCapture(video_path)
 # if not cap.isOpened():
 #     print('无法打开视频文件')
 #     exit()
-
+#
 # # 创建 VideoWriter 对象，用于保存处理后的视频
 # fps = cap.get(cv2.CAP_PROP_FPS)
 # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 # height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 # out = cv2.VideoWriter('processed_video.mp4', fourcc, fps, (width, height))
-
+#
 # # 逐帧处理视频
 # while cap.isOpened():
 #     ret, frame = cap.read()
 #     if not ret:
 #         break
-
+#
 #     # 调用 process_frame() 函数处理视频帧
 #     processed_frame, _ = face.process_frame(frame)
-    
+#
 #     # 将处理后的帧写入到 VideoWriter 对象中
 #     out.write(processed_frame)
-
+#
 # # 释放视频对象
 # cap.release()
 # out.release()
-
+#
 # print('视频处理完成，并保存为 processed_video.mp4 文件')
 
 
@@ -105,8 +105,6 @@ def process_video(video_url):
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    # Prepare VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = io.BytesIO()
 
@@ -125,7 +123,10 @@ def process_video(video_url):
     # Release resources
     cap.release()
 
-    return out.getvalue()
+    # Encode the video stream to base64
+    video_stream = base64.b64encode(out.getvalue()).decode()
+
+    return video_stream
 
 if __name__ == "__main__":
     # Parse command line arguments
