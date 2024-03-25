6b97b705ee20eac87d8f44517b3841fc3d6c4051
zhoujessie <zhouljeasc@163.com>
2024-03-25 16:05:48 -0700

    frontend: record api fetch

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
+const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
+
+
+const checkIsSameDay = (d1: Date | null, d2: Date | null): boolean => {
+  if (!d1 || !d2) return false
+  return d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate()
+}
+
+export { checkIsSameDay, cn }
